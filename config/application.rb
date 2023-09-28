fb1a92a835216249a63c4dcf757b66f5d4f64f83
Oliver Coimbra <olivercoimbra@gmail.com>
2023-09-28 12:11:13 -0400

    Fix deploy errrors

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,8 @@
 
 require_relative 'boot'
 
-# require 'rails/all'
-require 'sprockets/railtie'
+require 'rails/all'
+# require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
